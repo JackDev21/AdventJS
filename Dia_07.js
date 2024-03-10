1d306d17e73d704ac0cf7c1688c102b1fe54718a
JackDev21 <jose.a.c.lopez@gmail.com>
2024-03-10 11:47:25 +0100

    dia 7

--- Dia_07.js
+++ Dia_07.js
@@ -39,29 +39,34 @@ Importante: Nos han dicho que siempre hay que dejar un salto de línea al final 
   Nota: Ten en cuenta que, en los tests, la primera línea se ve empujada por el caracter ". */
 
 function drawGift(size, symbol) {
-  if (size === 1) return "#\n"
+  // Si el tamaño es 1, devuelve un solo símbolo de regalo y un salto de línea
+  if (size === 1) return "#\n";
 
+  // Inicializa un array para almacenar las filas del regalo
   let result = [""];
 
-  // Dibujar la parte superior 
-  result[0] = "#".repeat(size).padStart(size * 2 - 1, " ")
+  // Dibuja la parte superior del regalo
+  result[0] = "#".repeat(size).padStart(size * 2 - 1, " ");
 
-  //Dibujar la parte inferior
-  result[size * 2 - 2] = "#".repeat(size)
+  // Dibuja la parte inferior del regalo
+  result[size * 2 - 2] = "#".repeat(size);
 
-  //Dibujar parte central
-  result[size - 1] = `${'#'.repeat(size)}${symbol.repeat(size - 2)}#`
+  // Dibuja la parte central del regalo
+  result[size - 1] = `${'#'.repeat(size)}${symbol.repeat(size - 2)}#`;
 
-
-  //Dibujar resto de filas
+  // Dibuja el resto de las filas del regalo
   for (let fila = 1; fila < size - 1; fila++) {
-    let draft = `#${symbol.repeat(size - 2)}#${symbol.repeat(fila - 1)}#`
+    // Construye la fila actual del regalo
+    let draft = `#${symbol.repeat(size - 2)}#${symbol.repeat(fila - 1)}#`;
 
-    result[fila] = draft.padStart(size * 2 - 1, " ")
-    result[size * 2 - 2 - fila] = draft
+    // Inserta la fila actual en el resultado, ajustando el espaciado
+    result[fila] = draft.padStart(size * 2 - 1, " ");
+
+    // Inserta la fila opuesta en el resultado, ajustando el espaciado
+    result[size * 2 - 2 - fila] = draft;
   }
 
-
+  // Retorna el resultado como una cadena de texto con saltos de línea
   return result.join("\n").concat("\n");
 }
 
